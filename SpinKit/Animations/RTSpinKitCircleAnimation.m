f0d02c1d10dbf1c43c46b2eeb12d27685b58a553
raymondjavaxx <raymondjavaxx@gmail.com>
2014-09-08 12:10:56 -0400

    Swap animation

--- SpinKit/Animations/RTSpinKitCircleAnimation.m
+++ SpinKit/Animations/RTSpinKitCircleAnimation.m
@@ -29,41 +29,43 @@
 -(void)setupSpinKitAnimationInLayer:(CALayer*)layer withSize:(CGSize)size color:(UIColor*)color
 {
     NSTimeInterval beginTime = CACurrentMediaTime();
-    
-    CGFloat radius = size.width / 2;
-    CGFloat squareSize = size.width / 6;
 
-    for (NSInteger i=0; i < 12; i+=1) {
-        CALayer *square = [CALayer layer];
-        
-        CGFloat angle = i * (M_PI_2/3.0);
+    CGFloat radius = size.width / 2;
+    CGFloat circleSize = size.width / 4;
+
+    for (NSInteger i=0; i < 8; i+=1) {
+        CALayer *circle = [CALayer layer];
+  
+        CGFloat angle = i * M_PI_4;
         CGFloat x = radius + sinf(angle) * radius;
         CGFloat y = radius - cosf(angle) * radius;
-        square.frame = CGRectMake(x, y, squareSize, squareSize);
-        square.backgroundColor = color.CGColor;
-        square.anchorPoint = CGPointMake(0.5, 0.5);
-        square.opacity = 0.0;
-
-        CATransform3D transform = CATransform3DIdentity;
-        transform = CATransform3DRotate(transform, angle, 0, 0, 1.0);
-        square.transform = transform;
-
-        CAKeyframeAnimation *anim = [CAKeyframeAnimation animationWithKeyPath:@"opacity"];
+        circle.frame = CGRectMake(x, y, circleSize, circleSize);
+        circle.backgroundColor = color.CGColor;
+        circle.anchorPoint = CGPointMake(0.5, 0.5);
+        circle.cornerRadius = CGRectGetHeight(circle.bounds) * 0.5;
+        circle.transform = CATransform3DMakeScale(0.0, 0.0, 0.0);
+  
+        CAKeyframeAnimation *anim = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
         anim.removedOnCompletion = NO;
         anim.repeatCount = HUGE_VALF;
         anim.duration = 1.0;
-        anim.beginTime = beginTime + (0.084 * i);
+        anim.beginTime = beginTime + (0.125 * i);
         anim.keyTimes = @[@(0.0), @(0.5), @(1.0)];
-        
+  
         anim.timingFunctions = @[
+            [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
             [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
             [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]
         ];
-        
-        anim.values = @[@(1.0), @(0.0), @(0.0)];
 
-        [layer addSublayer:square];
-        [square addAnimation:anim forKey:@"spinkit-anim"];
+        anim.values = @[
+            [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)],
+            [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0, 1.0, 0.0)],
+            [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)]
+        ];
+
+        [layer addSublayer:circle];
+        [circle addAnimation:anim forKey:@"spinkit-anim"];
     }
 }
 
